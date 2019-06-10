1cab18efe9d2496ab071745b68079822c74eb295
XiaoLuo01 <fuxiaoluo01@163.com>
2019-06-10 15:23:44 +0800

    实现基本的计算功能

--- renderProcess/index.js
+++ renderProcess/index.js
@@ -1,5 +1,6 @@
 // 引入 ipcRenderer
 const ipcRenderer = require('electron').ipcRenderer
+var math = require('mathjs')
 
 // 获取展示结果的文本框
 var resultText = document.querySelector('.result-text')
@@ -50,3 +51,72 @@ ipcRenderer.on('c_default', (event, color) => {
     resultText.style.fontSize = '50px'
     localStorage.setItem('sysFontSize', 50)
 })
+
+// 实现计算功能
+let result = ''
+let main = {
+    // 添加标记, 标记用户是否点击了 = 号
+    isEqual: false,
+    // 标记用户是否按下运算符
+    isOpt: false,
+    // 实现数字的输入
+    clickNum(num) {
+        // 判断用户是否刚刚点击了 =
+        if (this.isEqual && !this.isOpt) {
+            result = ''
+            resultText.innerHTML = '0'
+            this.isEqual = false
+        }
+        // 判断用户是否重复输入了点
+        let isPiont = num === '.'
+        if (result.indexOf('.') !== -1 && isPiont) {
+            return
+        }
+        // 将用户的输入依次拼接到 result 中
+        result = result.toString()
+        result += num
+        // 将 result 值复制给文本框
+        resultText.innerHTML = result
+    },
+    // 重置用户的输入
+    reset() {
+        resultText.innerHTML = '0'
+        result = ''
+    },
+    // 每次点击删除最后一个
+    delete() {
+        result = result.substring(0, result.length -1)
+        resultText.innerHTML = result.length === 0 ? '0' : result
+    },
+    // 实现计算功能
+    clickopt(opt) {
+        switch (opt) {
+            case '+/-':
+                result = math.eval(result + "*-1")
+                resultText.innerHTML = result
+                break;
+            case '%':
+                result = math.format(math.eval(result + "/100"), 4)
+                resultText.innerHTML = result
+                break;
+            default:
+                result = result + opt
+                resultText.innerHTML = result
+                break;
+        }
+        this.isOpt = true
+    },
+    // 实现最终的计算功能
+    calc() {
+        result = math.eval(result).toString()
+        resultText.innerHTML = result
+        this.isEqual = true
+        this.isOpt = false
+    }
+}
+
+// // 单机右键弹出右键菜单(上下文菜单)
+// document.oncontextmenu = () => {
+//     // 渲染进程向主进程发送消息
+//     ipcRenderer.send('c_showContextMenu')
+// }
